8135556674628ae5136278ff2146198fc7e8e7f5
pedro felipe <pedrofelipinho@gmail.com>
2024-04-26 15:29:26 -0300

    Criando o titulo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Breadcrumb from "./Componentes/BreadCrumb"
 import Cabecalho from "./Componentes/Cabecalho"
 import GlobalStyles from "./Componentes/EstiloGlobal"
+import CadastroMeteorologico from "./Componentes/Titulo"
 
 const App = () => (
     <>
       <GlobalStyles />
           <Cabecalho />
           <Breadcrumb />
+          <CadastroMeteorologico />
     </>
   )
 
---
+++ src/Componentes/Titulo/index.jsx
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 43px;
+    width: 565px;
+    height: 44px;
+    position: absolute;
+    top: 174px;
+    left: 120px;
+`
+
+const Texto = styled.span`
+    font-family: 'TTSupermolot-Bold';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 36px;
+    line-height: 44.46px;
+
+    color: #292929;
+
+    flex: none;
+    order: 0;
+    flex-grow: 0;
+`
+
+const CadastroMeteorologico = () => {
+    return (
+        <Container>
+            <Texto>Cadastro de Dados Meteorológicos</Texto>
+        </Container>
+    )
+}
+
+export default CadastroMeteorologico;
