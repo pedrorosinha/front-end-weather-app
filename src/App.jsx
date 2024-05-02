a4a70f996e154dc9830256d7f02baa1335cc2353
pedro felipe <pedrofelipinho@gmail.com>
2024-05-02 16:58:43 -0300

    Criando o componente da data e o calendário

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Breadcrumb from "./Componentes/BreadCrumb"
 import Cabecalho from "./Componentes/Cabecalho"
 import GlobalStyles from "./Componentes/EstiloGlobal"
 import InputBusca from "./Componentes/InputBusca"
+import InputData from "./Componentes/InputData"
 import InputTemperatura from "./Componentes/InputTemperatura"
 import InputTempo from "./Componentes/InputTempo"
 import CadastroMeteorologico from "./Componentes/Titulo"
@@ -15,6 +16,7 @@ const App = () => (
           <InputBusca />
           <InputTemperatura />
           <InputTempo />
+          <InputData />
     </>
   )
 
---
+++ src/Componentes/InputData/index.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import { DatePicker } from "antd";
+import styled from "styled-components";
+import locale from "antd/es/date-picker/locale/pt_BR";
+
+const Container = styled.div`
+  position: relative;
+  margin-bottom: 16px;
+`;
+
+const Titulo = styled.div`
+  position: absolute;
+  width: 236px;
+  height: 39px;
+  left: 709px;
+  top: 261px;
+  font-family: 'TTSupermolot-Regular';
+  font-size: 32px;
+  font-weight: 400;
+  line-height: 39px;
+  color: #292929;
+`;
+
+const DateContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 8px;
+  position: absolute;
+  width: 200px;
+  height: 68px;
+  left: 709px;
+  top: 316px;
+`;
+
+const Subtitulo = styled.div`
+  width: 45px;
+  height: 22px;
+  font-family: 'TTSupermolot-Regular';
+  font-size: 18px;
+  font-weight: 400;
+  line-height: 22px;
+  color: #292929;
+`;
+
+const StyledDatePicker = styled(DatePicker)`
+  width: 100%;
+  height: 40px;
+  border-radius: 6px;
+  border: 2px solid #414ABA;
+  display: flex;
+  align-items: center;
+  padding: 8px 12px;
+  font-family: 'TTSupermolot-Regular';
+  font-size: 18px;
+  font-weight: 400;
+  line-height: 22px;
+  color: #292929;
+  .ant-picker-suffix {
+    color: #414ABA;
+  }
+`;
+
+const InputData = () => {
+    return (
+        <Container>
+            <Titulo>Selecione a data</Titulo>
+            <DateContainer>
+                <Subtitulo>Data*</Subtitulo>
+                <StyledDatePicker placeholder="Selecione a data" format='DD/MM/YYYY' locale={locale}/>
+            </DateContainer>
+        </Container>
+    );
+};
+
+export default InputData;
