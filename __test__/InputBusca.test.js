6da706820d3f1930d490b73c9c808c3f6177c043
pedro felipe <pedrofelipinho@gmail.com>
2024-05-17 17:32:01 -0300

    Adicionando um teste de erro no input de busca

--- __test__/InputBusca.test.js
+++ __test__/InputBusca.test.js
@@ -11,4 +11,15 @@ describe('Componente do input de buscar a cidade', () => {
 
     expect(input.value).toBe('Porto Alegre');
   });
+  
+  it('Deve exibir uma mensagem de erro se nenhum valor for fornecido para o campo de busca', () => {
+    const { getByTestId, getByText } = render(<InputBusca />);
+    const input = getByTestId('input-busca');
+
+    fireEvent.change(input, { target: { value: '' } }); 
+
+    const errorMessage = getByText('Informe a cidade'); 
+
+    expect(errorMessage).toBeInTheDocument();
+  });
 });
