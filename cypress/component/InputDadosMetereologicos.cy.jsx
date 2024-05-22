d9b71d876e628ce5e2c8992c8c2c3f95a72d3072
pedro felipe <pedrofelipinho@gmail.com>
2024-05-22 09:02:32 -0300

    Criando o teste do input dos dados metereológicos

---
+++ cypress/component/InputDadosMetereologicos.cy.jsx
@@ -0,0 +1,23 @@
+/// <reference types="cypress" />
+
+import React from "react";
+import { mount } from "cypress/react";
+import InputDadosMetereologicos from "../../src/Componentes/InputDadosMetereologicos";
+
+describe('InputDadosMetereologicos Component', () => {
+  it('Deve renderizar os campos de entrada com os placeholders corretos', () => {
+    mount(<InputDadosMetereologicos />);
+
+    cy.get('[data-testid="input-precipitacao"]').should('have.attr', 'placeholder', 'Digite aqui');
+    cy.get('[data-testid="input-umidade"]').should('have.attr', 'placeholder', 'Digite aqui');
+    cy.get('[data-testid="input-velocidade-vento"]').should('have.attr', 'placeholder', 'Digite aqui');
+  });
+
+  it('Deve chamar a função onInputChange ao alterar os valores dos campos de entrada', () => {
+    const onInputChangeSpy = cy.spy().as('onInputChangeSpy');
+    mount(<InputDadosMetereologicos onInputChange={onInputChangeSpy} />);
+
+    cy.get('[data-testid="input-precipitacao"]').type('10');
+    cy.get('@onInputChangeSpy').should('have.been.called');
+  });
+});
